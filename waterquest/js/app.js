c1b7e3971f8aea5cf9dc10f6bed2fb6b247a08ce
Arnaud Masselin <web@adfinitas.fr>
2016-08-10 08:08:31 +0000

    Updated '/waterquest/js/app.js' via CloudCannon

--- waterquest/js/app.js
+++ waterquest/js/app.js
@@ -61,6 +61,7 @@ function showAnswer() {
   var result = getResult();
   $(".name").html(pureStr($("input[name='firstname']").val()) + " " +
 		  pureStr($("input[name='lastname']").val()));
+		  console.log(result);
   $("form#myForm").slideUp();
   $("#result-" + result).slideDown();
   $(".result-part").slideDown(400, function() {
